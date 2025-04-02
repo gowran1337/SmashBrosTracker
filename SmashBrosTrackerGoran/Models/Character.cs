16440fdb7c6b5100ca335917de0618c6f108bc7b
Goran Nadiri <Goran.nadiri@hotmail.com>
2025-04-02 16:57:51 +0200

    functioning css for show character image on charselect screen

--- SmashBrosTrackerGoran/Models/Character.cs
+++ SmashBrosTrackerGoran/Models/Character.cs
@@ -5,7 +5,7 @@
         public int Id { get; set; }  // Primary Key
         public string Name { get; set; }  // Character Name (e.g., "Fox", "Marth", etc.)
 
-
+        public string? ImageUrl { get; set; }  // URL to an image of the character
 
         // Navigation property: List of players who use this character
         public List<Player> Players { get; set; } = new List<Player>();
